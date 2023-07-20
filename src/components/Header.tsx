68f63d61d956afc3a56a02f1a1d303ecb143cf18
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-20 15:58:40 +0300

    fix logo import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import logo from '../assets/logo.svg';
+import {Logo } from "@/assets/logo"
 
 export const Header = () => {
   return (
     <div className="bg-gray-700 h-[200px] w-full flex flex-shrink-0 items-center justify-center">
-      <img src={logo} alt="logo" width={22} height={36} className="flex-shrink-0" />
+      <Logo />
       <div className="flex ml-1">
         <h1 className="text-blue text-3xl inline font-black">to</h1>
         <h1 className="text-purple-dark text-3xl inline font-black">do</h1>
