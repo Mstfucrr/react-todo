6c3cbb6659017c5f4afbb450f68d54d96ab233fc
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-20 12:54:27 +0300

    Added retrieval tasks from local storage and view tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import { Header } from "./components/Header";
 import { TaskInput } from "./components/TaskInput";
 import { TaskContainer } from "./components/TaskContainer";
+import { useEffect, useState } from "react";
+import Task  from "./model/Task";
+
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  // setTasks çalıştığında tasks'ı localStorage'a kaydet
+  
+  useEffect(() => {
+    const jsTasks = localStorage.getItem("tasks");
+    if (jsTasks) {
+      const storedTasks = JSON.parse(jsTasks) as Task[];
+      if (storedTasks) {
+        setTasks(storedTasks);
+      }
+    }
+  }, []);
 
   return (
     <div className="app">
       <Header />
-      <TaskInput />
-      <TaskContainer />
+      <TaskInput setTasks={setTasks} tasks={tasks} />
+      <TaskContainer setTasks={setTasks} tasks={tasks} />
     </div>
   )
 }
