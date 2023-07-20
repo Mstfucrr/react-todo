ec517e24c1c8cee5f5a5a535d5053c35df5caa65
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-20 12:53:11 +0300

    add task model

---
+++ src/model/Task.ts
@@ -0,0 +1,13 @@
+
+class Task {
+    constructor(
+        public id: number,
+        public title: string,
+        public status: boolean,
+        public createdAt: string,
+    ) { }
+
+
+}
+
+export default Task;
